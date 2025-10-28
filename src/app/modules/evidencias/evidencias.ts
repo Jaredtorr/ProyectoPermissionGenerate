4418e489cea66c0067c15704a8dd865b0a012481
JORED666 <243842@ids.upchiapas.edu.mx>
2025-10-27 22:30:38 -0600

    feature: Vista de evidencias

---
+++ src/app/modules/evidencias/evidencias.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evidencias',
+  imports: [],
+  standalone: true,
+  templateUrl: './evidencias.html',
+  styleUrl: './evidencias.css',
+})
+export class Evidencias {
+
+}
