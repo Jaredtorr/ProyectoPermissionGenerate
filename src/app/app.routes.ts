2831e25745ee8f392e3877e79cb2b6f0ea7f0032
Jaredtorr <233310@ids.upchiapas.edu.mx>
2025-10-27 20:54:14 -0600

    tutorados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { GeneratePermissionComponent } from './modules/generate-permission/generate-permission.component';
+import { TutoradosComponent } from './modules/tutorados/tutorados.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent, children: [
-        { path: 'generate-permission', component: GeneratePermissionComponent }
+        { path: 'generate-permission', component: GeneratePermissionComponent },
+        { path: 'tutorados', component: TutoradosComponent }
     ]}
 ];
