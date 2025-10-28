88f2d92025591bcb75a1b8b943aa9252811f0f71
JORED666 <243842@ids.upchiapas.edu.mx>
2025-10-27 23:45:50 -0600

    fix: redireccionado a la vista de welcome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Evidencias } from './modules/evidencias/evidencias';
 import { WelcomeComponent } from './modules/welcome/welcome.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard/welcome', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent, children: [
         { path: 'generate-permission', component: GeneratePermissionComponent },
         { path: 'tutorados', component: TutoradosComponent },
