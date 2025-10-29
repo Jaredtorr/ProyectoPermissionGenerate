9421fd554fb441ac8f943378e6f7dcbbb94c2851
ivanGG23 <243836@ids.upchiapas.edu.mx>
2025-10-28 20:48:22 -0600

    view register view

---
+++ src/app/modules/registro/registro.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registro',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
+})
+export class RegistroComponent {
+  constructor(private router: Router) {}
+
+  sentToLogin(event: Event) {
+    event.preventDefault();
+    this.router.navigate(['']);
+  }
+}
